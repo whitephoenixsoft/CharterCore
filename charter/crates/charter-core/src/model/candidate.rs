f61eea54f7fdf8478e8e22973f3631388284eecf
whitephoenixsoft <whitephoenixapps@gmail.com>
2025-12-26 21:56:39 -0500

    Added candidate model code

---
+++ charter/crates/charter-core/src/model/candidate.rs
@@ -0,0 +1,15 @@
+use crate::ids::CandidateId;
+use crate::time::Timestamp;
+
+#[derive(Debug, Clone)]
+pub struct Candidate {
+    pub id: CandidateId,
+
+    /// Semantical information being voted upon
+    pub content: String,
+
+    /// The reason why it was proposed
+    pub rationale: Option<String>,
+
+    pub created_at: Timestamp,
+}
