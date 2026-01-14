848405c2c2b9a44649e7073948570a81c430375a
whitephoenixsoft <whitephoenixapps@gmail.com>
2026-01-14 11:34:16 -0500

    added cli template code

--- charter/crates/charter-cli/src/main.rs
+++ charter/crates/charter-cli/src/main.rs
@@ -1,3 +1,40 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(name = "charter")]
+#[command(about = "A CLI to version control your decisions.", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Save the current engine state to a file NOT REAL
+    Save {
+        /// The path to the file where state will be saved
+        path: PathBuf,
+    },
+    /// Load a state from a file (example of another command) NOT REAL
+    Load {
+        path: PathBuf,
+    },
+}
+
 fn main() {
-    println!("{}", charter_core::engine_version());
+    println!("NOT OFFICIAL CLI; PLACEHOLDER CODE")
+    println!("charter-cli 0.1.0; {}", charter_core::engine_version());
+    
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Save { path } => {
+            println!("Saving engine state to: {:?}", path);
+            // logic to call your object store goes here
+        }
+        Commands::Load { path } => {
+            println!("Loading state from: {:?}", path);
+        }
+    }
 }
