5b58f866e8d81100a54e0ef9396d8cd32136f8db
whitephoenixsoft <whitephoenixapps@gmail.com>
2026-01-23 12:53:59 -0500

    updated areaid to use uuid

--- charter/crates/charter-core/src/model/ids.rs
+++ charter/crates/charter-core/src/model/ids.rs
@@ -1,21 +1,37 @@
+use uuid::Uuid;
+use derive_more::{Display, From, FromStr};
 use serde::Serialize;
 
-#[derive(Clone, PartialEq, Eq, Hash, Debug, Serialize)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Display, Serialize)]
 #[serde(transparent)]
-pub struct AreaId(pub String);
+pub struct AreaId(pub Uuid);
 
-#[derive(Clone, PartialEq, Eq, Hash, Debug, Serialize)]
+impl AreaId {
+    pub fn new() -> Self {
+        Self(Uuid::now_v7())
+    }
+}
+
+impl std::str::FromStr for AreaId {
+    type Err = uuid::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(Uuid::parse_str(s)?))
+    }
+}
+
+
+#[derive(Clone, PartialEq, Eq, Hash, Debug, Serialize, From, FromStr, Display)]
 #[serde(transparent)]
 pub struct SessionId(pub String);
 
-#[derive(Clone, PartialEq, Eq, Hash, Debug, Serialize)]
+#[derive(Clone, PartialEq, Eq, Hash, Debug, Serialize, From, FromStr, Display)]
 #[serde(transparent)]
 pub struct ResolutionId(pub String);
 
-#[derive(Clone, PartialEq, Eq, Hash, Debug, Serialize)]
+#[derive(Clone, PartialEq, Eq, Hash, Debug, Serialize, From, FromStr, Display)]
 #[serde(transparent)]
 pub struct CandidateId(pub String);
 
-#[derive(Clone, PartialEq, Eq, Hash, Debug, Serialize)]
+#[derive(Clone, PartialEq, Eq, Hash, Debug, Serialize, From, FromStr, Display)]
 #[serde(transparent)]
 pub struct ActorId(pub String);
